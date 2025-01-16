f4fd5089260cb5357af917a430f6cc25959c4d18
Amal-10a <161098167+Amal-10a@users.noreply.github.com>
2025-01-16 03:05:15 +0300

    add dark mode
    make it user friendly and add dark mode

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 function addToCart(productName) {
     alert(productName + " has been added to your cart.");
 }
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+    const themeIcon = document.querySelector(".theme-toggle i");
+    themeIcon.classList.toggle("fa-moon");
+    themeIcon.classList.toggle("fa-sun");
+}
